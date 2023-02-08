759992234e946036e5938746a1e09118b377e281
OmriBenHur <omribhk2000@gmail.com>
2023-02-08 13:45:24 +0200

    initialize git repo

---
+++ terraform/providers.tf
@@ -0,0 +1,27 @@
+# using terraform cloud to run these configuration files.
+# aws access key and secret key are saved as env variables in the tf cloud workspace. so no declaration in code is needed
+terraform {
+  cloud {
+    organization = "omribenhur"
+
+    workspaces {
+      name = "eks-modules"
+    }
+  }
+
+  required_providers {
+    kubectl = {
+      source = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = ">= 2.6.0"
+    }
+  }
+}
+
+# provider conf. enter the region you're operating in
+provider "aws" {
+  region = "us-west-2"
+}
