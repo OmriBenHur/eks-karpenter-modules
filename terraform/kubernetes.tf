9e618311f8605c45dbb67cf4104fd5e8bbd4f07f
OmriBenHur <omribhk2000@gmail.com>
2023-02-12 16:58:53 +0200

    d

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,73 +1,73 @@
-#resource "kubectl_manifest" "karpenter_provisioner" {
-#  yaml_body = <<-YAML
-#    apiVersion: karpenter.sh/v1alpha5
-#    kind: Provisioner
-#    metadata:
-#      name: default
-#    spec:
-#      requirements:
-#        - key: karpenter.sh/capacity-type
-#          operator: In
-#          values: ${var.karpenter-capacity-type}
-#        - key: node.kubernetes.io/instance-type
-#          operator: In
-#          values: ${var.karpenter-instance-type}
-#      limits:
-#        resources:
-#          cpu: ${var.karpenter-cpu-limit}
-#      providerRef:
-#        name: default
-#      ttlSecondsAfterEmpty: ${var.karpenter-ttl-empty}
-#  YAML
-#
-#  depends_on = [helm_release.karpenter]
-#}
-#
-#resource "kubectl_manifest" "karpenter_node_template" {
-#  yaml_body = <<-YAML
-#    apiVersion: karpenter.k8s.aws/v1alpha1
-#    kind: AWSNodeTemplate
-#    metadata:
-#      name: default
-#    spec:
-#      subnetSelector:
-#        ${var.karpenter-subnet-tag}: "true"
-#      securityGroupSelector:
-#        karpenter.sh/discovery: ${module.eks.cluster_name}
-#      tags:
-#        karpenter.sh/discovery: ${module.eks.cluster_name}
-#  YAML
-#
-#  depends_on = [kubectl_manifest.karpenter_provisioner]
-#}
-#
-#resource "kubectl_manifest" "karpenter_example_deployment" {
-#  yaml_body = <<-YAML
-#    apiVersion: apps/v1
-#    kind: Deployment
-#    metadata:
-#      name: mongodb-deployment
-#      labels:
-#        app: mongodb
-#    spec:
-#      replicas: 3
-#      selector:
-#        matchLabels:
-#          app: mongodb
-#      template:
-#        metadata:
-#          labels:
-#            app: mongodb
-#        spec:
-#          containers:
-#          - name: mongodb
-#            image: mongo
-#            ports:
-#            - containerPort: 27017
-#            resources:
-#              requests:
-#                cpu: 2
-#  YAML
-#
-#  depends_on = [kubectl_manifest.karpenter_node_template]
-#}
+resource "kubectl_manifest" "karpenter_provisioner" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.sh/v1alpha5
+    kind: Provisioner
+    metadata:
+      name: default
+    spec:
+      requirements:
+        - key: karpenter.sh/capacity-type
+          operator: In
+          values: ${var.karpenter-capacity-type}
+        - key: node.kubernetes.io/instance-type
+          operator: In
+          values: ${var.karpenter-instance-type}
+      limits:
+        resources:
+          cpu: ${var.karpenter-cpu-limit}
+      providerRef:
+        name: default
+      ttlSecondsAfterEmpty: ${var.karpenter-ttl-empty}
+  YAML
+
+  depends_on = [helm_release.karpenter]
+}
+
+resource "kubectl_manifest" "karpenter_node_template" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.k8s.aws/v1alpha1
+    kind: AWSNodeTemplate
+    metadata:
+      name: default
+    spec:
+      subnetSelector:
+        ${var.karpenter-subnet-tag}: "true"
+      securityGroupSelector:
+        karpenter.sh/discovery: ${module.eks.cluster_name}
+      tags:
+        karpenter.sh/discovery: ${module.eks.cluster_name}
+  YAML
+
+  depends_on = [kubectl_manifest.karpenter_provisioner]
+}
+
+resource "kubectl_manifest" "karpenter_example_deployment" {
+  yaml_body = <<-YAML
+    apiVersion: apps/v1
+    kind: Deployment
+    metadata:
+      name: mongodb-deployment
+      labels:
+        app: mongodb
+    spec:
+      replicas: 3
+      selector:
+        matchLabels:
+          app: mongodb
+      template:
+        metadata:
+          labels:
+            app: mongodb
+        spec:
+          containers:
+          - name: mongodb
+            image: mongo
+            ports:
+            - containerPort: 27017
+            resources:
+              requests:
+                cpu: 2
+  YAML
+
+  depends_on = [kubectl_manifest.karpenter_node_template]
+}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -24,17 +24,17 @@ terraform {
     }
   }
 }
-#
-#data "aws_eks_cluster_auth" "cluster-auth" {
-#  name = var.cluster-name
-#  depends_on = [module.eks]
-#}
-#
-#data "aws_eks_cluster" "cluster" {
-#  name = var.cluster-name
-#  depends_on = [module.eks]
-#
-#}
+
+data "aws_eks_cluster_auth" "cluster-auth" {
+  name = var.cluster-name
+  depends_on = [module.eks]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster-name
+  depends_on = [module.eks]
+
+}
 
 data "aws_partition" "current" {}
 
@@ -52,21 +52,21 @@ provider "aws" {
   region = "us-east-1"
   alias  = "virginia"
 }
-#
-#provider "helm" {
-#  kubernetes {
-#    host                   = data.aws_eks_cluster.cluster.endpoint
-#    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-#    token                  = data.aws_eks_cluster_auth.cluster-auth.token
-#  }
-#}
-#
-#provider "kubectl" {
-#  host                   = data.aws_eks_cluster.cluster.endpoint
-#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-#  token                  = data.aws_eks_cluster_auth.cluster-auth.token
-#  load_config_file       = false
-#}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster-auth.token
+  }
+}
+
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster-auth.token
+  load_config_file       = false
+}
 
 locals {
   azs       = slice(data.aws_availability_zones.available_zones.names, 0, var.az-amount)
