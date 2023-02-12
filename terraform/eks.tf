c796706fc38702568de4a4e0045fce3eab99d625
OmriBenHur <omribhk2000@gmail.com>
2023-02-12 16:54:08 +0200

    d

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,39 +1,39 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.7.0"
-
-  cluster_name    = var.cluster-name
-  cluster_version = var.cluster-version
-
-  cluster_endpoint_public_access  = true
-  cluster_endpoint_private_access = true
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  # Required for Karpenter role below
-  enable_irsa = true
-
-  node_security_group_tags = {
-    # NOTE - if creating multiple security groups with this module, only tag the
-    # security group that Karpenter should utilize with the following tag
-    # (i.e. - at most, only one security group should have this tag in your account)
-    "karpenter.sh/discovery" = var.cluster-name
-  }
-
-  # Only need one node to get Karpenter up and running.
-  # This ensures core services such as VPC CNI, CoreDNS, etc. are up and running
-  # so that Karpenter can be deployed and start managing compute capacity as required
-  eks_managed_node_groups = {
-    initial = {
-      instance_types = var.initial-instance-types
-      # Not required nor used - avoid tagging two security groups with same tag as well
-      create_security_group = false
-
-      # Ensure enough capacity to run 2 Karpenter pods
-      min_size     = var.node-group-min
-      max_size     = var.node-group-max
-      desired_size = var.node-group-des
-    }
-  }
-}
+#module "eks" {
+#  source  = "terraform-aws-modules/eks/aws"
+#  version = "19.7.0"
+#
+#  cluster_name    = var.cluster-name
+#  cluster_version = var.cluster-version
+#
+#  cluster_endpoint_public_access  = true
+#  cluster_endpoint_private_access = true
+#
+#  vpc_id     = module.vpc.vpc_id
+#  subnet_ids = module.vpc.private_subnets
+#
+#  # Required for Karpenter role below
+#  enable_irsa = true
+#
+#  node_security_group_tags = {
+#    # NOTE - if creating multiple security groups with this module, only tag the
+#    # security group that Karpenter should utilize with the following tag
+#    # (i.e. - at most, only one security group should have this tag in your account)
+#    "karpenter.sh/discovery" = var.cluster-name
+#  }
+#
+#  # Only need one node to get Karpenter up and running.
+#  # This ensures core services such as VPC CNI, CoreDNS, etc. are up and running
+#  # so that Karpenter can be deployed and start managing compute capacity as required
+#  eks_managed_node_groups = {
+#    initial = {
+#      instance_types = var.initial-instance-types
+#      # Not required nor used - avoid tagging two security groups with same tag as well
+#      create_security_group = false
+#
+#      # Ensure enough capacity to run 2 Karpenter pods
+#      min_size     = var.node-group-min
+#      max_size     = var.node-group-max
+#      desired_size = var.node-group-des
+#    }
+#  }
+#}
