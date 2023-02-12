df3ecf1524a3c97bf65a773ace33c8eb98a4020b
OmriBenHur <omribhk2000@gmail.com>
2023-02-12 14:13:07 +0200

    provisioner

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -9,12 +9,12 @@ resource "kubectl_manifest" "karpenter_provisioner" {
         - key: node.kubernetes.io/instance-type
           operator: In
           values: ${var.karpenter-capacity-type}
-        - key: karpenter.k8s.aws/instance-category
-          operator: In
-          values: ${var.karpenter-instance-category}
-        - key: karpenter.k8s.aws/instance-generation
-          operator: Gt
-          values: ${var.karpenter-instance-generation}
+#        - key: karpenter.k8s.aws/instance-category
+#          operator: In
+#          values: ${var.karpenter-instance-category}
+#        - key: karpenter.k8s.aws/instance-generation
+#          operator: Gt
+#          values: ${var.karpenter-instance-generation}
       limits:
         resources:
           cpu: ${var.karpenter-cpu-limit}
@@ -70,7 +70,7 @@ resource "kubectl_manifest" "karpenter_example_deployment" {
             - containerPort: 27017
             resources:
               requests:
-                cpu: 1
+                cpu: 2
   YAML
 
   depends_on = [kubectl_manifest.karpenter_node_template]
